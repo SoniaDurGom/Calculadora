9be306896984225751e0e4adcc2fe1ee97e741e1
SoniaDurGom <soniaduran97@hotmail.es>
2022-04-18 12:10:53 +0200

    Esqueleto

--- Testing/Calculadora.test.js
+++ Testing/Calculadora.test.js
@@ -4,17 +4,20 @@ describe("Calculadora.js",()=> {  //Descripcion del programa
     test ("Test numero SUMA: ", ()=>{ //Test suma
         expect(Calculadora.sumar(3,1)).toBe(4); 
         expect(Calculadora.sumar(-3,1)).toBe(-2); 
+        expect(Calculadora.sumar(3,-1)).toBe(2); 
         //expect(sumar(1,5)).toBe(6);
     })
 
     test ("Test numero RESTA: ", ()=>{ //Test resta
         expect(Calculadora.restar(5,3)).toBe(2);
         expect(Calculadora.restar(-3,5)).toBe(-8);
+        expect(Calculadora.restar(3,-5)).toBe(-2);
     })
     
     test ("Test numero MULTIPLICACIÓN: ", ()=>{ //Test Multiplicacion
        expect(Calculadora.multiplicar(2,5)).toBe(10);
        expect(Calculadora.multiplicar(-2,5)).toBe(-10);
+       expect(Calculadora.multiplicar(2,-5)).toBe(-10);
        expect(Calculadora.multiplicar(-2,-5)).toBe(+10);
     })
 
