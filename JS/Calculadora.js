88af8c1cd7abb0e6eb130ebd56e1b8dd03f83a2c
SoniaDurGom <soniaduran97@hotmail.es>
2022-04-19 21:33:03 +0200

    Commit

--- JS/Calculadora.js
+++ JS/Calculadora.js
@@ -26,10 +26,15 @@ function calcular (){ //LLamado cuando se pulsa "=" CALCULAR RESULTADO
             break;
     }
     return resultado; //Devuelve resultado de la operacion.
+    console.log(resultado);
 }
 //Mostrar resultado: Con el igual
 function mostrar (){
     console.log(resultado); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!Mostrar resultado por intefaz, no consola
+}
+
+function imprimir (){
+    
 }
 
 //Obtener numero: Si no hay ningun numero guardado guardarlo como num1
@@ -38,11 +43,12 @@ function mostrar (){
 function obtenerNumero (numero){ //Llamada al pulsar cualquier boton numerico
     if (num1!=null && num2==null){
         num2=numero;
-        
+        console.log(num2);
         return num2;
     }
     if (num1==null){
         num1=numero;
+        console.log(num1);
         return num1;
     }
     else{
@@ -54,25 +60,31 @@ function obtenerNumero (numero){ //Llamada al pulsar cualquier boton numerico
 function obtenerOperador (op){
     if(op == '+' || op == '-' || op ==  '*' || op ==  '/'){
         operador=op;
+        console.log(operador);
         return operador;
     }
     else{
+        console.log(operador);
         return "ERROR"
     }
+    
 }
 
 function sumar(){
     resultado=num1+num2;
+    console.log(resultado);
     return resultado;
 }
 
 function restar (){
     resultado=num1-num2;
+    console.log(resultado);
     return resultado;
 }
 
 function multiplicar (){
     resultado=num1*num2;
+    console.log(resultado);
     return resultado;
 }
 
@@ -82,6 +94,7 @@ function dividir (){
     }else{
         resultado="NAN";
     }
+    console.log(resultado);
     return resultado;
 }
 
@@ -92,12 +105,6 @@ function borrar (){
     operador=null;
     resultado=null;
 }
-
-
-
-
-
-
 
 //cdobject.onclick = function(){myScript};
 
