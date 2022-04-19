8179a2d7e79e7bbfab9222a16f60ae413709761c
SoniaDurGom <soniaduran97@hotmail.es>
2022-04-19 22:08:18 +0200

    Imprimir

--- JS/Calculadora.js
+++ JS/Calculadora.js
@@ -25,16 +25,17 @@ function calcular (){ //LLamado cuando se pulsa "=" CALCULAR RESULTADO
             resultado="NAN";
             break;
     }
+    //console.log(resultado);
+    document.getElementById('display').innerHTML = resultado;
     return resultado; //Devuelve resultado de la operacion.
-    console.log(resultado);
+   
 }
+
 //Mostrar resultado: Con el igual
 function mostrar (){
-    console.log(resultado); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!Mostrar resultado por intefaz, no consola
 }
 
 function imprimir (){
-    
 }
 
 //Obtener numero: Si no hay ningun numero guardado guardarlo como num1
@@ -43,16 +44,19 @@ function imprimir (){
 function obtenerNumero (numero){ //Llamada al pulsar cualquier boton numerico
     if (num1!=null && num2==null){
         num2=numero;
-        console.log(num2);
+        //console.log(num2);
+        document.getElementById('display').innerHTML = num2;
         return num2;
     }
     if (num1==null){
         num1=numero;
-        console.log(num1);
+        //console.log(num1);
+        document.getElementById('display').innerHTML = num1;
         return num1;
     }
     else{
-        return "ERROR"
+        document.getElementById('display').innerHTML = "ERROR";
+        return "ERROR";
     }
     
 }
@@ -60,19 +64,21 @@ function obtenerNumero (numero){ //Llamada al pulsar cualquier boton numerico
 function obtenerOperador (op){
     if(op == '+' || op == '-' || op ==  '*' || op ==  '/'){
         operador=op;
-        console.log(operador);
+        //console.log(operador);
+        document.getElementById('display').innerHTML = operador;
         return operador;
     }
     else{
-        console.log(operador);
-        return "ERROR"
+        //console.log(operador);
+        document.getElementById('display').innerHTML = "ERROR";
+        return "ERROR";
     }
     
 }
 
 function sumar(){
     resultado=num1+num2;
-    console.log(resultado);
+    //console.log(resultado);
     return resultado;
 }
 
@@ -104,14 +110,9 @@ function borrar (){
     num2=null;
     operador=null;
     resultado=null;
+    document.getElementById('display').innerHTML = resultado;
 }
 
 //cdobject.onclick = function(){myScript};
 
 module.exports= {sumar, restar, dividir, multiplicar,borrar,calcular,obtenerNumero,obtenerOperador,num1,num2,operador,resultado}
-//module.exports.mostrar = mostrar;
-//module.exports.borrar = borrar;
-//odule.exports.multiplicar = multiplicar;
-//module.exports.sumar = sumar;
-//module.exports.restar = restar;
-//module.exports.dividir = dividir;
