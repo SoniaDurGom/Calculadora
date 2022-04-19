be490511022141a8b629ccc57a94ade6fccca431
SoniaDurGom <soniaduran97@hotmail.es>
2022-04-19 17:20:20 +0200

    CAmbio en el testing

--- Testing/Calculadora.test.js
+++ Testing/Calculadora.test.js
@@ -1,32 +1,144 @@
 const Calculadora = require("../JS/Calculadora"); //Importar programa
 
 describe("Calculadora.js",()=> {  //Descripcion del programa
-    test ("Test numero SUMA: ", ()=>{ //Test suma
-        expect(Calculadora.sumar(3,1)).toBe(4); 
-        expect(Calculadora.sumar(-3,1)).toBe(-2); 
-        expect(Calculadora.sumar(3,-1)).toBe(2); 
-        //expect(sumar(1,5)).toBe(6);
+
+    test ("Test obtenerNumero: ", ()=>{ //Test suma
+        expect(Calculadora.obtenerNumero(1)).toBe(1); 
+        expect(Calculadora.obtenerNumero(2)).toBe(2); 
+        expect(Calculadora.obtenerNumero(52)).toBe("ERROR"); 
+    })
+
+    test ("Test numero optenerOperador: ", ()=>{ //Test suma
+        expect(Calculadora.obtenerOperador('+')).toBe('+'); 
+        expect(Calculadora.obtenerOperador('-')).toBe('-'); 
+        expect(Calculadora.obtenerOperador('*')).toBe('*'); 
+        expect(Calculadora.obtenerOperador('/')).toBe('/');
+        expect(Calculadora.obtenerOperador(':')).toBe("ERROR");
+    })
+
+    test ("Test borrar: ", ()=>{ 
+       Calculadora.borrar();
+    })
+
+    test ("Test numero SUMA: ", ()=>{ //Test resta
+        Calculadora.obtenerNumero(1);
+        Calculadora.obtenerNumero(2);
+        expect(Calculadora.sumar()).toBe(3);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(-5);
+        Calculadora.obtenerNumero(3);
+        expect(Calculadora.sumar()).toBe(-2);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(1);
+        Calculadora.obtenerNumero(-2);
+        expect(Calculadora.sumar()).toBe(-1);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(1);
+        Calculadora.obtenerNumero(2);
+        expect(Calculadora.sumar()).toBe(3);
+        Calculadora.borrar();
     })
 
     test ("Test numero RESTA: ", ()=>{ //Test resta
-        expect(Calculadora.restar(5,3)).toBe(2);
-        expect(Calculadora.restar(-3,5)).toBe(-8);
-        expect(Calculadora.restar(3,-5)).toBe(8);
+        Calculadora.obtenerNumero(10);
+        Calculadora.obtenerNumero(5);
+        expect(Calculadora.restar()).toBe(5);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(-5);
+        Calculadora.obtenerNumero(10);
+        expect(Calculadora.restar()).toBe(-15);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(5);
+        Calculadora.obtenerNumero(-10);
+        expect(Calculadora.restar()).toBe(15);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(-5);
+        Calculadora.obtenerNumero(-10);
+        expect(Calculadora.restar()).toBe(5);
+        Calculadora.borrar();
+
     })
     
     test ("Test numero MULTIPLICACIÓN: ", ()=>{ //Test Multiplicacion
-       expect(Calculadora.multiplicar(2,5)).toBe(10);
-       expect(Calculadora.multiplicar(-2,5)).toBe(-10);
-       expect(Calculadora.multiplicar(2,-5)).toBe(-10);
-       expect(Calculadora.multiplicar(-2,-5)).toBe(+10);
+        Calculadora.obtenerNumero(10);
+        Calculadora.obtenerNumero(5);
+        expect(Calculadora.multiplicar()).toBe(50);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(-1);
+        Calculadora.obtenerNumero(5);
+        expect(Calculadora.multiplicar()).toBe(-5);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(-1);
+        Calculadora.obtenerNumero(-5);
+        expect(Calculadora.multiplicar()).toBe(5);
+        Calculadora.borrar();
     })
 
     test ("Test numero DIVISIÓN: ", ()=>{ //Test division
-        expect(Calculadora.dividir(15,5)).toBe(3); 
-        //expect(Calculadora.aux).toBe("3"); 
-        expect(Calculadora.dividir(0,5)).toBe("NAN"); 
-      
+        Calculadora.obtenerNumero(10);
+        Calculadora.obtenerNumero(5);
+        expect(Calculadora.dividir()).toBe(2);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(-15);
+        Calculadora.obtenerNumero(3);
+        expect(Calculadora.dividir()).toBe(-5);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(-25);
+        Calculadora.obtenerNumero(-5);
+        expect(Calculadora.dividir()).toBe(5);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(0);
+        Calculadora.obtenerNumero(2);
+        expect(Calculadora.dividir()).toBe(0); 
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(10);
+        Calculadora.obtenerNumero(0);
+        expect(Calculadora.dividir()).toBe("NAN"); 
+        Calculadora.borrar();
     })
 
+    test ("Test numero calcular: ", ()=>{ 
+        Calculadora.obtenerNumero(5);
+        Calculadora.obtenerNumero(3);
+        Calculadora.obtenerOperador('+');
+        expect(Calculadora.calcular()).toBe(8);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(5);
+        Calculadora.obtenerNumero(3);
+        Calculadora.obtenerOperador('-');
+        expect(Calculadora.calcular()).toBe(2);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(5);
+        Calculadora.obtenerNumero(3);
+        Calculadora.obtenerOperador('*');
+        expect(Calculadora.calcular()).toBe(15);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(15);
+        Calculadora.obtenerNumero(3);
+        Calculadora.obtenerOperador('/');
+        expect(Calculadora.calcular()).toBe(5);
+        Calculadora.borrar();
+
+        Calculadora.obtenerNumero(5);
+        Calculadora.obtenerNumero(3);
+        Calculadora.obtenerOperador('()');
+        expect(Calculadora.calcular()).toBe("NAN");
+        Calculadora.borrar();
+    })
 
 })
